df34ad025d4572b08d1094abac9e24e633cba668
Spongie Uncle <geosam2708@gmail.com>
2021-12-18 00:29:56 +0300

    completed task 2

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
           @Override
           public void onSuccess(int statusCode, Header[] headers, String response) {
             Log.d("AsyncHttpClient", "response = " + response);
-            Gson gson = new GsonBuilder().create();;
+            Gson gson = new GsonBuilder().create();
             candies = gson.fromJson(response, Candy[].class);
 
             addCandiesToDatabase(candies);
@@ -86,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
   // ***
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    public boolean onOptionsItemSelected( MenuItem item) {
       Intent infoIntent = new Intent(this, InfoActivity.class);
       startActivity(infoIntent);
         return super.onOptionsItemSelected(item);
